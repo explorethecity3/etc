ff9fa4ede15581eb61b1ecdc542e3fba0f3bd03b
maheshr29 <mahesh.r@royalcyber.com>
2025-04-21 15:58:48 +0530

    latest changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 ">
       <h1 className="text-center text-4xl font-bold text-blue-800 tracking-[1.5rem]">EXPLORE THE CITY</h1>
-      <h3 className="text-center text-2xl mt-2 text-violet-800 tracking-[0.5rem] mt-20">Coming Soon.....</h3>
+      <h1 className="text-center text-2xl mt-2 text-violet-800 tracking-[0.5rem] mt-20">Coming Soon.....</h1>
     </div>
   );
 }
